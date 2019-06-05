1fff77dd38deacb49e2aae19530ffd388e6e7cba
Dionysius Indraatmadja <dionysius.indraatmadja@mail.utoronto.ca>
2019-06-05 12:36:12 -0400

    Update sample_scanning.py

--- py/sample_scanning.py
+++ py/sample_scanning.py
@@ -3,6 +3,13 @@
 ## Started June 5 2019
 
 import numpy as np
+#####################################################################################
+## Define global constants
+#####################################################################################
+TOP_LEFT = (0, 0)
+TOP_RIGHT = (0, -1)
+BOTTOM_LEFT = (-1, 0)
+BOTTOM_RIGHT = (-1, -1)
 
 class Scan():
      #####################################################################################
@@ -11,9 +18,9 @@ class Scan():
      ## This represents 2D scanning in a rectangular area.
      ######################################################################################
      
-     def __init__(self):
+     def __init__(self, DIMENSIONS=(3,3), START_POS=""):
           #####################################################################################
-          ## Define constants
+          ## Define class constants
           #####################################################################################
           self.TOP_LEFT = (0, 0)
           self.TOP_RIGHT = (0, -1)
@@ -24,17 +31,17 @@ class Scan():
           self.up = 999
           self.down = -999
           self.STEP_DICT = {self.left: "-x", self.right: "x",self.up:"+y", self.down:"-y"}
+          POS_DICT = {"top left": TOP_LEFT, "top right": TOP_RIGHT, 
+                      "bottom left": BOTTOM_LEFT, "bottom right": BOTTOM_RIGHT}                      
           self.arr = np.array([])
           #####################################################################################
           ## EDIT THESE PARAMETERS
-          self.SAMPLE_DIMENSIONS= (5,5)  ## (rows, columns)
-          self.START_POS = self.TOP_RIGHT  ## starting position of transducer
+          self.SAMPLE_DIMENSIONS= DIMENSIONS  ## (rows, columns)
+          self.START_POS = POS_DICT[START_POS]  ## starting position of transducer
           self.X_STEP_SIZE = 100  ## step along a row
           self.Y_STEP_SIZE = 50  ## step along a column
-          #####################################################################################
-          self.STEP_PARAM()
+          
     
-     
      def STEP_PARAM(self):
           #####################################################################################
           ## Determine start and end positions
@@ -70,9 +77,10 @@ class Scan():
                     
           else:
                print("Unexpected Error")
-               
+          
+          arr = np.zeros(SAMPLE_DIMENSIONS)  ## tuple
           if self.START_POS == self.TOP_RIGHT:
-               arr = np.zeros(SAMPLE_DIMENSIONS)  ## tuple
+               
                PHASE = 1
                for y in range(SAMPLE_DIMENSIONS[0]):
                     if (y % 2) == 0:
@@ -85,9 +93,9 @@ class Scan():
                          arr[y, -1] = self.VERTICAL_STEP
                
                arr[self.END_POS]= 0
-          
+               del PHASE
+               
           elif self.START_POS == self.TOP_LEFT:
-               arr = np.zeros(SAMPLE_DIMENSIONS)  ## tuple
                PHASE = 0
                for y in range(SAMPLE_DIMENSIONS[0]):
                     if (y % 2) == 0:
@@ -99,15 +107,16 @@ class Scan():
                          arr[y, 1:] = (-1)**(y + PHASE)
                          arr[y, 0] = self.VERTICAL_STEP
                arr[self.END_POS] = 0
-          
+               del PHASE
+               
           elif self.START_POS == self.BOTTOM_LEFT:
                pass  ## work on this
           elif self.START_POS == self.BOTTOM_RIGHT:
                pass  ## work on this
                
           self.arr = np.copy(arr)
-          del arr, PHASE
-     
+          del arr
+               
      
      def STEP(self, DIRECTION='+x'):
           #####################################################################################
@@ -137,6 +146,7 @@ class Scan():
           #####################################################################################
           ## Walk through array and call on STEP_DICT functions to move motor, work on this
           #####################################################################################
+          self.STEP_PARAM()
           pos = self.START_POS
           while self.arr[pos] != 0:
                V = self.arr[pos]
@@ -156,8 +166,11 @@ class Scan():
 
           del pos, V
           
-
+     def __repr__(self):
+          ## String representation of the sample area
+          return np.array2string(self.arr)
                     
 if __name__ == '__main__':
-     trial = Scan()
+     trial = Scan(DIMENSIONS=(100, 100),START_POS="top left")
      trial.start()
+     a = trial
