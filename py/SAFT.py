955b508d9778da689557f7edbb7460f95430f430
Dionysius Indraatmadja <43018518+dnysius@users.noreply.github.com>
2019-07-05 13:05:16 -0400

    Update SAFT.py

--- py/SAFT.py
+++ py/SAFT.py
@@ -11,12 +11,13 @@ from os import getcwd
 from os.path import join, dirname
 import pickle
 from tqdm import tqdm
-from time import clock
+from time import thread_time_ns
+import matplotlib.pyplot as plt
 global min_step, c_0, DEFAULT_ARR_FOLDER
 global xarr, FD, SD, pbar, T, V, L, T_COMPARE, PRE_OUT, POST_OUT, xni
 FOLDER_NAME = "1D-15FOC3in"
-#DEFAULT_ARR_FOLDER = join(dirname(getcwd()), "data", FOLDER_NAME)
-DEFAULT_ARR_FOLDER = getcwd()
+DEFAULT_ARR_FOLDER = join(dirname(getcwd()), "data", FOLDER_NAME)
+#DEFAULT_ARR_FOLDER = getcwd()
 FOCAL_DEPTH = 0.0381  # 1.5 inch in metres
 SAMPLE_DEPTH = 15000
 min_step = 4e-4
@@ -51,48 +52,57 @@ L = np.shape(V)[1]
 T_COMPARE = np.empty((L, len(T)))
 for l in range(L):
     T_COMPARE[l, :] = T[:]
-xarr = np.linspace(-L/2, L/2, L)*min_step
 PRE = np.flip(V[:FD, :], axis=0)
 PRE_T = np.flip(T[:FD], axis=0)
 PRE_OUT = np.empty(np.shape(PRE))
 POST = V[FD:SD, :]
 POST_T = T[FD:SD]
 POST_OUT = np.empty(np.shape(POST))
+xarr = np.linspace(-L/2, L/2, L)*min_step
 xni = np.arange(0, L, 1)
 xi = 0
 pbar = tqdm(total=SD*L)
 tstep = np.mean(T[1:]-T[:-1])
-start = 0
-while xi < 1:
+t1 = thread_time_ns()
+end = 0
+while xi < L:
     x = xarr[xi]
     ti = 0
+    start = 0
     while ti < SD:
         pbar.update(1)
-        pbar.set_description('xi {0}, ti {1}, speed {2}:\t'.format(xi, ti,
-                             clock()-start))
         z = T[ti]*c_0/2
+        z2 = np_power(z, 2)
         # one iteration of this loop takes 0.1 s... BORING!
-        start = clock()
         if ti < FD:  # PRE
             ind = ((2/c_0)*np_sqrt(np_power(x-xarr[xni], 2)
-                   + np_power(z, 2))).reshape((L, 1))
+                    + z2)).reshape((L, 1))
 #            zi = np_abs(T_COMPARE - ind).argmin(axis=1)  # more accurate
-            # less accurate, computationally efficient
-            zi = np.floor(ind/tstep).astype(int)
-            PRE_OUT[ti, xi] = np_sum(V[zi, xi])
+            zi = np.floor(ind/tstep).astype(int)  # less accurate, computationally efficient
+            PRE_OUT[ti, xi] = np_sum(V[zi[zi<FD], xi])  # zi<SD performs SAFT on PRE and POST
         elif ti >= FD:  # POST
             ind = ((2/c_0)*np_sqrt(np_power(x-xarr[xni], 2)
-                   + np_power(z, 2))).reshape((L, 1))
+                    + z2)).reshape((L, 1))
 #            zi = np_abs(T_COMPARE - ind).argmin(axis=1)  # more accurate
-            # less accurate, computationally efficient
-            zi = np.floor(ind/tstep).astype(int)
-            POST_OUT[ti-FD, xi] = np_sum(V[zi, xi])
+            zi = np.floor(ind/tstep).astype(int)  # less accurate, computationally efficient
+            POST_OUT[ti-FD, xi] = np_sum(V[zi[zi<SD], xi])
         ti += 1
+        pbar.set_description('xi {0}, ti {1}, speed {2}:\t'.format(xi, ti, end-start))
+    end = thread_time_ns() - start
     xi += 1
 
+dt1 = thread_time_ns()-t1
 pbar.close()
-
 PRE_OUT = np.flip(PRE_OUT, axis=0)
 STITCHED = np.vstack((PRE_OUT, POST_OUT))
-pickle.dump(STITCHED, open(join(getcwd(),"{}-test.pkl".format(FOLDER_NAME))
-                           , "wb"))
+pickle.dump(STITCHED, open(join(DEFAULT_ARR_FOLDER,"SAFT-{}-test.pkl".format(FOLDER_NAME)), "wb"))
+print('Loop time: {}'.format(dt1))
+
+#fig = plt.figure(figsize=[2,10])
+#plt.imshow(STITCHED[:, 0:1], aspect='auto', cmap='gist_stern')
+#plt.show()
+
+#fig = plt.figure(figsize=[2,10])
+#plt.imshow(STITCHED[:, 0:1], aspect='auto', cmap='hot', vmin=0)
+#plt.colorbar()
+#plt.show()
