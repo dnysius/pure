379cac0945040ab09d06bb64aa6e5dcc704af7fe
Dionysius Indraatmadja <dionysius.indraatmadja@mail.utoronto.ca>
2019-07-18 13:34:08 -0400

    fixed mistakes in multi_SAFT

--- py/multi_SAFT.py
+++ py/multi_SAFT.py
@@ -15,11 +15,9 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 global min_step, c_0, DEFAULT_ARR_FOLDER
 global xarr, FD, SD, pbar, T, V, L, T_COMPARE, PRE_OUT, POST_OUT, xni
-FOLDER_NAME = "1D-3FOC3in"
+FOLDER_NAME = "1D-15FOC3in"
 DEFAULT_ARR_FOLDER = join(dirname(getcwd()), "data", FOLDER_NAME)
-#DEFAULT_ARR_FOLDER = getcwd()
-FOCAL_DEPTH = 0.0381*2  # 1.5 inch in metres
-SAMPLE_DEPTH = 15000
+FOCAL_DEPTH = 0.0381  # 1.5 inch in metres
 min_step = 4e-4
 c_0 = 1498  # water
 
@@ -48,6 +46,7 @@ V = varr[ZERO:, :]  # 2D
 FD = find_nearest(T, 2*FOCAL_DEPTH/c_0)  # focal depth
 # SD = find_nearest(T, 2*SAMPLE_DEPTH/c_0) + 1  # sample depth
 SD = len(T)-1
+OFFSET = T[FD]
 L = np.shape(V)[1]
 PRE = np.flip(V[:FD, :], axis=0)
 PRE_T = np.flip(T[:FD], axis=0)
@@ -58,49 +57,45 @@ POST_OUT = np.empty(np.shape(POST))
 xarr = np.linspace(-L/2, L/2, L)*min_step
 xni = np.arange(0, L, 1)
 tstep = np.mean(T[1:]-T[:-1])
-#while xi < L:
-def main(xi):
-    x = xarr[xi]
-    ti = 0
+
+
+def main(xi):  # xi is imaging index/position
+    x = xarr[xi]  # x is imaging DISTANCE
+    ti = 0  # imaging pixel for time/ z direction
     while ti < SD:
         z = T[ti]*c_0/2
-        z2 = np_power(z, 2)
+        z2 = np_power(z, 2)  # distance, squared
         if ti < FD:  # PRE
-            ind = ((2/c_0)*np_sqrt(np_power(x-xarr[xni], 2)
-                   + z2)).reshape((L, 1))
-            zi = np.floor(ind/tstep).astype(int)
-            PRE_OUT[ti, xi] = np_sum(V[zi[zi < FD], :])
+            ind = (2/c_0)*np_sqrt(np_power(x-xarr[xni], 2)
+                                  + z2)
+            zi = (ind/tstep).astype(int)
+            PRE_OUT[ti, xi] = np_sum(V[zi[zi < FD], xni[zi < FD]])
         if ti >= FD:  # POST
-            ind = ((2/c_0)*np_sqrt(np_power(x-xarr[xni], 2)
-                   + z2)).reshape((L, 1))
-            zi = np.floor(ind/tstep).astype(int)
-            POST_OUT[ti-FD, xi] = np_sum(V[zi[zi < SD], :])
+            ind = (2/c_0)*np_sqrt(np_power(x-xarr[xni], 2)
+                   + z2)  # in time
+            # interpolation
+            zi = (ind/tstep).astype(int)
+            POST_OUT[ti-FD, xi] = np_sum(V[zi[zi < SD], xni[zi < SD]])
         ti += 1
 
 if __name__=='__main__':
     # Parallel processing
     jobs = []
-    pbar = tqdm(total=L)
+    print("Append")
     for i in range(L):
-        pbar.update(1)
         jobs.append(threading.Thread(target=main, args=(i,)))
-    pbar.close()
+    print("Starting")
     for job in jobs:
         job.start()
-    pbar = tqdm(total=L)
-    pbar.set_description('Joining\n')
+    print("Joining")
     for job in jobs:
-        pbar.update(L)
         job.join()
-    pbar.close()
-PRE_OUT = np.flip(PRE_OUT, axis=0)
-#pickle.dump(POST_OUT, open(join(DEFAULT_ARR_FOLDER, "SAFT-{}-post.pkl".format(FOLDER_NAME)), "wb"))
-#pickle.dump(PRE_OUT, open(join(DEFAULT_ARR_FOLDER, "SAFT-{}-pre.pkl".format(FOLDER_NAME)), "wb"))
-
-STITCHED = np.vstack((PRE_OUT, POST_OUT))
-pickle.dump(STITCHED, open(join(DEFAULT_ARR_FOLDER,"SAFT-{}-test.pkl".format(FOLDER_NAME)), "wb"))
-
-fig = plt.figure(figsize=[10,10])
-plt.imshow(STITCHED[:,:], aspect='auto', cmap='hot')
-plt.colorbar()
-plt.show()
+    PRE_OUT = np.flip(PRE_OUT, axis=0)
+    STITCHED = np.vstack((PRE_OUT, POST_OUT))
+    pickle.dump(STITCHED, open(join(DEFAULT_ARR_FOLDER,
+                                    "SAFT-{}-test.pkl"
+                                    .format(FOLDER_NAME)), "wb"))
+    fig = plt.figure(figsize=[10, 10])
+    plt.imshow(STITCHED[:, :], aspect='auto', cmap='hot')
+    plt.colorbar()
+    plt.show()
