cdc34e2e0b3a0d9c5c897c9c3d319eba764e1d9a
Dionysius Indraatmadja <dionysius.indraatmadja@mail.utoronto.ca>
2019-08-06 11:09:04 -0400

    hilbert with axis=0

--- py/analysis_SAFT.py
+++ py/analysis_SAFT.py
@@ -87,7 +87,7 @@ def bscan(i="", figsize=[8, 8], start=0, end=-1, y1=0, y2=-1, sa=True):
         b = b[:, :]
     else:
         b = b[:, :]
-    plt.imshow(np.log10(np.abs(hilbert(b[start:end, :]))), aspect='auto', cmap='gray')
+    plt.imshow(np.log10(np.abs(hilbert(b[start:end, :], axis=0))), aspect='auto', cmap='gray')
 #    plt.colorbar()
     plt.axhline(y=y1)
     plt.axhline(y=y2)
