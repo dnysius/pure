ccaed48548358ddd54875dd0df69ebc9954458ad
Dionysius Indraatmadja <dionysius.indraatmadja@mail.utoronto.ca>
2019-09-08 00:18:44 -0400

    Update 1d_bscan.py

--- py/1d_bscan.py
+++ py/1d_bscan.py
@@ -5,10 +5,11 @@ from scipy.signal import hilbert
 import matplotlib.pyplot as plt
 from os import getcwd
 from os.path import join, dirname
-global min_step
+from matplotlib.ticker import FixedFormatter
+global min_step, FILENAME
 min_step = 4e-4
-FOLDER_NAME = "1D-3FOC7in"
-FILENAME = "scope"
+FOLDER_NAME = "1D-3FOC7in"  # edit this
+FILENAME = "SAFT-1D-3FOC7in.pkl"  # and this
 BSCAN_FOLDER = join(dirname(getcwd()), "scans", "BSCAN")
 if FOLDER_NAME[:2] == "2D":
     par = "2D SCANS"
@@ -21,51 +22,51 @@ SCAN_FOLDER = join(dirname(getcwd()), "data", par, FOLDER_NAME)
 
 def load_arr(output_folder=SCAN_FOLDER):
     ftarr = join(output_folder, "tarr.pkl")
-    fvarr = join(output_folder, "SAFT-1D-3FOC7in.pkl")
+    fvarr = join(output_folder, FILENAME)
     with open(ftarr, 'rb') as rd:
         tarr = pickle.load(rd)
     with open(fvarr, 'rb') as rd:
         varr = pickle.load(rd)
-        print(varr.shape)
     return tarr, varr
 
 
-def bscan(folder=SCAN_FOLDER, figsize=[0, 0], start=0, end=-1, y1=0, y2=-1, hil=True):
-    tarr, varr = load_arr(folder)
-    fig, ax1 = plt.subplots(1, 1, figsize=(10, 10))
-    if y2 == -1:
-        y2 = len(varr[start:end, 0]) - 1
-#    b = varr[:, 0, :]
-    b = varr
-    timestep = np.mean(tarr[1:, :, :] - tarr[:-1, :, :])
-    print(timestep)
-    if hil is True:
-        b = np.abs(hilbert(b, axis=0))
-        b = 20*np.log10(b/np.max(b.flatten()))
-        b = b[start:end, :]
-    plt.imshow(b, aspect='auto', cmap='gray', vmin=-60, interpolation='none')
-    ax1.set_xticklabels(np.round(ax1.get_xticks()*100*min_step, 4))
-    ax1.set_yticklabels(np.round((ax1.get_yticks()+start)*100*timestep*1498/2, 2))
-#    plt.axhline(y=y1, label='{}'.format(y1+start))
-#    plt.axhline(y=y2, label='{}'.format(y2+start))
-    dt = np.mean(tarr[y2+start, :, :]) - np.mean(tarr[y1+start, :, :])
+def bscan(folder=SCAN_FOLDER, figsize=[0, 0], start=0, end=-1, y1=0, y2=-1):
+    global ax1, ax2, timestep
     v_w = 1498
     v_m = 6420
+    tarr, varr = load_arr(folder)
+    timestep = np.mean(tarr[1:, :, :] - tarr[:-1, :, :])  # get avg timestep
+    fig, ax1 = plt.subplots(1, 1, figsize=(14, 10))
+    if y2 == -1:
+        y2 = len(varr[start:end, 0]) - 1
+    b = np.abs(hilbert(varr, axis=0))  # b = varr[:, 0, :]
+    b = 20*np.log10(b/np.max(b.flatten()))
+    b = b[start:end, :]
+    plt.imshow(b, aspect='auto', cmap='gray', vmin=-60, vmax=0, interpolation='none', alpha=0)
+    ax1.set_xticklabels(np.round(ax1.get_xticks()*100*min_step, 4))
+    ax1.set_yticklabels((ax1.get_yticks()).astype(int))
+    dt = np.mean(tarr[y2+start, :, :]) - np.mean(tarr[y1+start, :, :])
     dw = v_w*dt/2
     dm = v_m*dt/2
-    plt.imshow(b, aspect='auto', cmap='gray', vmin=-60, interpolation='none')
-#    plt.axhline(y=0, label='water: {}'.format(dw), alpha=0)
-#    plt.axhline(y=0, label='aluminum: {}'.format(dm), alpha=0)
     plt.title("{0}".format(FOLDER_NAME))
-#    plt.legend()
-    plt.xlabel("lateral distance (cm)")
-    plt.ylabel("axial distance (cm)")
-    plt.colorbar()
+    ax2 = ax1.twinx()  # second scale on same axes
+    im = ax2.imshow(b, aspect='auto', cmap='gray', vmin=-60, vmax=0, interpolation='none')
+    fig.colorbar(im, orientation='vertical', pad=0.08)
+    plt.axhline(y=y1, label='{}'.format(y1+start))
+    plt.axhline(y=y2, label='{}'.format(y2+start))
+    plt.axhline(y=0, label='water: {} m'.format(np.round(dw, 5)), alpha=0)
+    plt.axhline(y=0, label='aluminum: {} m'.format(np.round(dm, 5)), alpha=0)
+    y_formatter = FixedFormatter(np.round((ax2.get_yticks()+start)*100*timestep*1498/2, 2))
+    ax2.yaxis.set_major_formatter(y_formatter)
+    ax2.set_xlabel("lateral distance (cm)")
+    ax2.set_ylabel("index")
+    ax2.set_ylabel("axial distance (cm)")
+    plt.legend()
     plt.show(fig)
 
 
-def ibscan(folder=SCAN_FOLDER, figsize=[10, 10], start=0, end=-1, y1=0, y2=-1, hil=True):
-    bscan(folder=folder, figsize=figsize, start=start, end=end, y1=y1, y2=y2, hil=hil)
+def ibscan(folder=SCAN_FOLDER, figsize=[14, 10], start=0, end=-1, y1=0, y2=-1):
+    bscan(folder=folder, figsize=figsize, start=start, end=end, y1=y1, y2=y2)
     cmd = input('//\t')
     if cmd == 'x':
         print('Exit')
@@ -82,7 +83,7 @@ def ibscan(folder=SCAN_FOLDER, figsize=[10, 10], start=0, end=-1, y1=0, y2=-1, h
                 a2 = y2
             else:
                 a2 = int(a2)
-            ibscan(folder=folder, figsize=figsize, start=start, end=end, y1=a1, y2=a2, hil=hil)
+            ibscan(folder=folder, figsize=figsize, start=start, end=end, y1=a1, y2=a2)
         except ValueError:
             print("Invalid input")
     elif cmd == 'z':
@@ -97,15 +98,11 @@ def ibscan(folder=SCAN_FOLDER, figsize=[10, 10], start=0, end=-1, y1=0, y2=-1, h
                 a2 = end
             else:
                 a2 = int(a2)
-            ibscan(folder=folder, figsize=figsize, start=a1, end=a2, y1=y1, y2=y2, hil=hil)
+            ibscan(folder=folder, figsize=figsize, start=a1, end=a2, y1=y1, y2=y2)
         except ValueError:
             print('invalid input')
-    elif cmd == 'raw' or cmd == 'r':
-        ibscan(folder=folder, figsize=figsize, start=start, end=end, y1=y1, y2=y2, hil=False)
-    elif cmd == 'hil' or cmd == 'h' or cmd == 'hilbert':
-        ibscan(folder=folder, figsize=figsize, start=start, end=end, y1=y1, y2=y2, hil=True)
     else:
-        ibscan(folder=folder, figsize=figsize, start=start, end=end, y1=y1, y2=y2, hil=hil)
+        ibscan(folder=folder, figsize=figsize, start=start, end=end, y1=y1, y2=y2)
 
 
 if __name__ == '__main__':
