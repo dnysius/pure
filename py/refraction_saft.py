31a8a480c73243b4a08815929ba2e94b7d81fc74
Dionysius Indraatmadja <dionysius.indraatmadja@mail.utoronto.ca>
2019-09-22 15:19:34 -0400

    fixed refraction, starting to implement to saft

--- py/refraction_saft.py
+++ py/refraction_saft.py
@@ -12,13 +12,14 @@ import scipy.optimize as so
 import pickle
 import matplotlib.pyplot as plt
 global min_step, c_0, DEFAULT_ARR_FOLDER
-global xarr, FD, SD, pbar, T, V, L, T_COMPARE, PRE_OUT, POST_OUT, xni
+global lat_distance, FD, SD, pbar, T, V, L, T_COMPARE, PRE_OUT, POST_OUT, lat_indices
 
-FOLDER_NAME = "1D-3FOC50cm"  # edit this
+FOLDER_NAME = "1D-3FOC5in-pipe-20um"  # edit this
 min_step = 4e-4  # and this
 FOCAL_DEPTH = 0.0381*2  # and this
 c_0 = 1498  # speed of sound in water
-
+SAMPLE_DEPTH = .5
+SAMPLE_THICKNESS = .013
 if FOLDER_NAME[:2] == "1D":
     par = "1D SCANS"
 else:
@@ -52,12 +53,12 @@ def disMod(z, aa, d2):
 
 d2 = np.linspace(.0001, 1.3, 100)  # discretize the sample thickness at sample depth
 tp = 250  # the number of transducer positions; lateral direction
-zw = np.zeros((d2, tp))
+zw = np.zeros((len(d2), tp))
 for i in range(tp):
     for j in range(len(d2)):
         zg = np.zeros((tp,))
         lat_distance = np.linspace(-5., 5., tp)  # 250 positions for -5cm to 5cm scan
-        z = so.fsolve(func=disMod, x0=zg[i], args=(lat_distance, d2(j)))  # solve function
+        z = so.fsolve(func=disMod, x0=zg[i], args=(lat_distance[i], d2[j]))  # solve function
         zw[j, i] = z
         # haven't found error analysis from fsolve
 
@@ -73,7 +74,7 @@ d1 = 50  # in cm, metal sample distance to the transducer
 td_water = d1/np.cos(np.deg2rad(angleW))  # real traveling distance inside water
 td_metal = np.zeros(zw.shape)
 for k in range(len(d2)):
-    temp = (d2(k)/100)/np.cos(np.deg2rad(angleM[k, :]))
+    temp = (d2[k]/100)/np.cos(np.deg2rad(angleM[k, :]))
     td_metal[k, :] = temp
 
 total_td = td_water + td_metal  # real traveling distance
@@ -86,7 +87,6 @@ T = tarr[ZERO:, 0]  # 1D, time columns all the same
 V = varr[ZERO:, :]  # 2D
 FD = find_nearest(T, 2*FOCAL_DEPTH/c_0)  # focal depth
 SD = len(T)
-OFFSET = T[FD]
 L = np.shape(V)[1]
 PRE = np.flip(V[:FD, :], axis=0)
 PRE_T = np.flip(T[:FD], axis=0)
@@ -94,50 +94,56 @@ PRE_OUT = np.empty(np.shape(PRE))
 POST = V[FD:SD, :]
 POST_T = T[FD:SD]
 POST_OUT = np.empty(np.shape(POST))
-xarr = np.linspace(-L/2, L/2, L)*min_step
-xni = np.arange(0, L, 1)
-tstep = np.mean(T[1:]-T[:-1])
+lat_distance = np.linspace(-L/2, L/2, L)*min_step
+lat_indices = np.arange(0, L, 1)
+tstep = np.mean(T[1:]-T[:-1])  # average timestep
+SAMPLE_DEPTH_INDEX = np.round(SAMPLE_DEPTH/tstep).astype(int)
+SAMPLE_THICKNESS_INDEX = np.round(SAMPLE_THICKNESS/tstep).astype(int)
+SAMPLE_RANGE = (SAMPLE_DEPTH_INDEX, SAMPLE_DEPTH_INDEX+SAMPLE_THICKNESS_INDEX)
+SAMPLE = np.zeros((SAMPLE_THICKNESS_INDEX, L), dtype=float)
+# if SAMPLE_RANGE[0] <= ti <=SAMPLE_RANGE[1]
+# then we need to apply the compensation for refraction in metal
 
-
-def main(xi):  # xi is imaging index/position
-    x = xarr[xi]  # x is imaging DISTANCE
+def main(lat_pixel):  # lat_pixel is imaging index/position
+    x = lat_distance[lat_pixel]  # x is imaging DISTANCE
     ti = 0  # imaging pixel for time/ z direction
     while ti < SD:
         z = T[ti]*c_0/2
         z2 = np_power(z, 2)  # distance, squared
-        ind = (2/c_0)*np_sqrt(np_power(x-xarr[xni], 2)
+        ind = (2/c_0)*np_sqrt(np_power(x-lat_distance[lat_indices], 2)
                               + z2)
-        zi = (ind/tstep).astype(int)
+        zi = np.round(ind/tstep).astype(int)
         if ti < FD:  # PRE
-            PRE_OUT[ti, xi] = np_sum(V[zi[zi < FD], xni[zi < FD]])
+            PRE_OUT[ti, lat_pixel] = np_sum(V[zi[zi < FD], lat_indices[zi < FD]])
         if ti >= FD:  # POST
-            POST_OUT[ti-FD, xi] = np_sum(V[zi[zi < SD], xni[zi < SD]])
+            POST_OUT[ti-FD, lat_pixel] = np_sum(V[zi[zi < SD], lat_indices[zi < SD]])
         ti += 1
 
 
 if __name__ == '__main__':
+    pass
     # Parallel processing
-    jobs = []
-    print("Append")
-    for i in range(L):
-        jobs.append(threading.Thread(target=main, args=(i,)))
-    print("Starting")
-    for job in jobs:
-        job.start()
-    print("Joining")
-    for job in jobs:
-        job.join()
-    print("Stitching")
-    PRE_OUT = np.flip(PRE_OUT, axis=0)
-    b = np.abs(hilbert(POST_OUT[:, :], axis=0))
-    b = 20*np.log10(b/np.max(b.flatten()))
-    STITCHED = np.vstack((PRE_OUT, b))
-    pickle.dump(STITCHED, open(join(DEFAULT_ARR_FOLDER,
-                                    "SAFT-{}.pkl"
-                                    .format(FOLDER_NAME)), "wb"))
+#    jobs = []
+#    print("Append")
+#    for i in range(L):
+#        jobs.append(threading.Thread(target=main, args=(i,)))
+#    print("Starting")
+#    for job in jobs:
+#        job.start()
+#    print("Joining")
+#    for job in jobs:
+#        job.join()
+#    print("Stitching")
+#    PRE_OUT = np.flip(PRE_OUT, axis=0)
+#    b = np.abs(hilbert(POST_OUT[:, :], axis=0))
+#    b = 20*np.log10(b/np.max(b.flatten()))
+#    STITCHED = np.vstack((PRE_OUT, b))
+#    pickle.dump(STITCHED, open(join(DEFAULT_ARR_FOLDER,
+#                                    "SAFT-{}.pkl"
+#                                    .format(FOLDER_NAME)), "wb"))
 
 
-fig = plt.figure(figsize=[10, 10])
-plt.imshow(STITCHED[:, :], aspect='auto', cmap='hot')
-plt.colorbar()
-plt.show()
+#fig = plt.figure(figsize=[10, 10])
+#plt.imshow(STITCHED[:, :], aspect='auto', cmap='hot')
+#plt.colorbar()
+#plt.show()
